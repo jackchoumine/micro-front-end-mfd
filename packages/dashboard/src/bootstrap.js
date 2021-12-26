f90092980d3416ed5cb12101507689305a4736d1
jack <jackzhoumine@gmail.com>
2021-12-27 02:11:31 +0800

    升级vue

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,22 +1,22 @@
-import { createApp } from 'vue';
-import Dashboard from './components/Dashboard.vue';
+import { createApp } from 'vue'
+import Dashboard from './components/Dashboard.vue'
 
 // Mount function to start up the app
 const mount = (el) => {
-  const app = createApp(Dashboard);
-  app.mount(el);
-};
+  const app = createApp(Dashboard)
+  app.mount(el)
+}
 
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_dashboard-dev-root');
+  const devRoot = document.querySelector('#dashboard-dev-root')
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot)
   }
 }
 
 // We are running through container
 // and we should export the mount function
-export { mount };
+export { mount }
