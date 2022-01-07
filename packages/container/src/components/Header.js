b154ddfed0e377aedfc024496d0546c184d574da
jack <jackzhoumine@gmail.com>
2022-01-07 21:40:41 +0800

    解决类名冲突

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link as RouterLink } from 'react-router-dom'
+
+export default function Header({ isSignedIn, onSignOut }) {
+  const classes = useStyles()
+
+  const onClick = () => {
+    if (isSignedIn && onSignOut) {
+      onSignOut()
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <AppBar position='static' color='default' elevation={0} className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant='h6' color='inherit' noWrap component={RouterLink} to='/'>
+            App
+          </Typography>
+          <Button
+            color='primary'
+            variant='outlined'
+            className={classes.link}
+            component={RouterLink}
+            to={isSignedIn ? '/' : '/auth/signin'}
+            onClick={onClick}
+          >
+            {isSignedIn ? 'Logout' : 'Login'}
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  )
+}
 
 const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -31,10 +63,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(8, 0, 6),
   },
   cardHeader: {
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? theme.palette.grey[200]
-        : theme.palette.grey[700],
+    backgroundColor: theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
   },
   cardPricing: {
     display: 'flex',
@@ -52,47 +81,4 @@ const useStyles = makeStyles((theme) => ({
       paddingBottom: theme.spacing(6),
     },
   },
-}));
-
-export default function Header({ isSignedIn, onSignOut }) {
-  const classes = useStyles();
-
-  const onClick = () => {
-    if (isSignedIn && onSignOut) {
-      onSignOut();
-    }
-  };
-
-  return (
-    <React.Fragment>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        className={classes.appBar}
-      >
-        <Toolbar className={classes.toolbar}>
-          <Typography
-            variant="h6"
-            color="inherit"
-            noWrap
-            component={RouterLink}
-            to="/"
-          >
-            App
-          </Typography>
-          <Button
-            color="primary"
-            variant="outlined"
-            className={classes.link}
-            component={RouterLink}
-            to={isSignedIn ? '/' : '/auth/signin'}
-            onClick={onClick}
-          >
-            {isSignedIn ? 'Logout' : 'Login'}
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
-  );
-}
+}))
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
+import React from 'react'
+import { Switch, Route, Router } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from './components/Landing'
+import Pricing from './components/Pricing'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'ma',
-});
+})
 
 export default ({ history }) => {
   return (
@@ -18,11 +15,11 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
+            <Route exact path='/pricing' component={Pricing} />
+            <Route path='/' component={Landing} />
           </Switch>
         </Router>
       </StylesProvider>
     </div>
-  );
-};
+  )
+}
